5732e2be849362519d1c674664703b6668ae9205
lesterli <fulfilself@gmail.com>
2023-08-06 15:41:43 +0000

    support for user's chainID

---
+++ docker/scripts/build-docker.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo docker build -t hermeznetwork/geth-zkevm-contracts:v1.1.0-snapchain -f docker/Dockerfile.geth .
--- docker/scripts/deploy-contracts.sh
+++ docker/scripts/deploy-contracts.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-sudo rm -rf docker/gethData/geth_data
-DEV_PERIOD=1 docker-compose -f docker/docker-compose.geth.yml up -d geth
-sleep 5
+
 node docker/scripts/fund-accounts.js
+echo "replace 1001 with $L2_CHAIN_ID"
+sed -i.bak "s/\"chainID\": 1001/\"chainID\": $L2_CHAIN_ID/g" docker/scripts/deploy_parameters_docker.json
 cp docker/scripts/deploy_parameters_docker.json deployment/deploy_parameters.json
 cp docker/scripts/genesis_docker.json deployment/genesis.json
 npx hardhat run deployment/testnet/prepareTestnet.js --network localhost
@@ -10,7 +10,3 @@ npx hardhat run deployment/2_deployPolygonZKEVMDeployer.js --network localhost
 npx hardhat run deployment/3_deployContracts.js --network localhost
 mkdir docker/deploymentOutput
 mv deployment/deploy_output.json docker/deploymentOutput
-docker-compose -f docker/docker-compose.geth.yml down
-sudo docker build -t hermeznetwork/geth-zkevm-contracts -f docker/Dockerfile.geth .
-# Let it readable for the multiplatform build coming later!
-sudo chmod -R go+rxw docker/gethData
