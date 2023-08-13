fc8c6adc19f03e06fefcd8130ff63e0806795fa6
lesterli <fulfilself@gmail.com>
2023-08-13 08:42:20 +0000

    allow customized deployer key

--- docker/scripts/deploy-contracts.sh
+++ docker/scripts/deploy-contracts.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-set -eux
+set -eu
 
 echo "JSONRPC_HTTP_URL: $JSONRPC_HTTP_URL"
 node docker/scripts/fund-accounts.js
 echo "Replace <chainID> with $L2_CHAIN_ID"
 sed -i.bak "s/<chainID>/$L2_CHAIN_ID/g" docker/scripts/deploy_parameters_docker.json
+echo "Replace <deployerPvtKey>"
+sed -i.bak "s/<deployerPvtKey>/$DEPLOYER_PRIVATE_KEY/g" docker/scripts/deploy_parameters_docker.json
+echo "Replace <deployerAddress> with $DEPLOYER_ADDRESS"
+sed -i.bak "s/<deployerAddress>/$DEPLOYER_ADDRESS/g" docker/scripts/deploy_parameters_docker.json
 cp docker/scripts/deploy_parameters_docker.json deployment/deploy_parameters.json
 cp docker/scripts/genesis_docker.json deployment/genesis.json
 npx hardhat run deployment/testnet/prepareTestnet.js --network zkevmMockL1Network
