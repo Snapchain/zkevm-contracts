23832c6fa6d99fd600f123d713a0dff9efb77432
lesterli <fulfilself@gmail.com>
2024-03-26 22:51:18 +0800

    add scripts from cdk-validium-contracts repo

---
+++ deployment/helpers/approve.js
@@ -0,0 +1,46 @@
+/* eslint-disable no-await-in-loop */
+/* eslint-disable no-console */
+
+require('dotenv').config();
+
+const deployOutput = require('../../docker/deploymentOutput/deploy_output.json');
+const createRollupOutput = require('../deploymentOutput/create_rollup_output.json');
+const polTokenContractJson = require('../../compiled-contracts/ERC20PermitMock.json');
+
+const DEFAULT_DEPLOYER_PRIVATE_KEY = '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80';
+
+async function main() {
+    const polTokenContractAddress = deployOutput.polTokenAddress;
+    if (!polTokenContractAddress) {
+        throw new Error(`Missing polTokenAddress: ${deployOutput}`);
+    }
+    const zkEVMContractAddress = createRollupOutput.rollupAddress;
+    if (!zkEVMContractAddress) {
+        throw new Error(`Missing rollupAddress: ${createRollupOutput}`);
+    }
+    // Load provider
+    const currentProvider = ethers.getDefaultProvider(`https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`);
+    const privateKey = process.env.DEPLOYER_PRIVATE_KEY || DEFAULT_DEPLOYER_PRIVATE_KEY;
+    // Load deployer
+    const deployer = new ethers.Wallet(privateKey, currentProvider);
+
+    const polTokenContract = new ethers.Contract(polTokenContractAddress, polTokenContractJson.abi, deployer);
+    const tx = await polTokenContract.approve(
+        zkEVMContractAddress,
+        ethers.utils.parseEther('100.0'),
+    );
+    console.log('Transaction hash:', tx.hash);
+    // Wait for receipt
+    const receipt = await tx.wait();
+    console.log('Transaction confirmed in block:', receipt.blockNumber);
+}
+
+// to prevent execution by accident on import
+if (require.main === module) {
+    main()
+        .then(() => process.exit(0))
+        .catch((error) => {
+            console.error(error);
+            process.exit(1);
+        });
+}
--- docker/scripts/set-sequencer-url.js
+++ docker/scripts/set-sequencer-url.js
@@ -1,24 +1,19 @@
 /* eslint-disable no-await-in-loop */
 /* eslint-disable no-console */
 
-const ethers = require('ethers');
-require('dotenv').config();
-
-const deployOutput = require('../deploymentOutput/create_rollup_output.json');
-const dataCommitteeContractJson = require('../../compiled-contracts/PolygonDataCommittee.json');
+const createRollupOutput = require('../deploymentOutput/create_rollup_output.json');
+const zkEVMContractJson = require('../../compiled-contracts/PolygonZkEVM.json');
 
 const DEFAULT_DEPLOYER_PRIVATE_KEY = '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80';
-const DEFAULT_DAC_ADDRESS = '0x70997970c51812dc3a010c7d01b50e0d17dc79c8';
 
 async function main() {
-    const dataCommitteeContractAddress = deployOutput.polygonDataCommitteeAddress;
-    if (!dataCommitteeContractAddress) {
-        throw new Error(`Missing DataCommitteeContract: ${deployOutput}`);
+    const zkEVMContractAddress = createRollupOutput.rollupAddress;
+    if (!zkEVMContractAddress) {
+        throw new Error(`Missing rollupAddress: ${createRollupOutput}`);
     }
-    const dataCommitteeUrl = process.env.DAC_URL || 'http://127.0.0.1:8444';
-    console.log('DAC_URL:', dataCommitteeUrl);
-    const dataCommitteeAddress = process.env.DAC_ADDRESS || DEFAULT_DAC_ADDRESS;
-    console.log('DAC_ADDRESS:', dataCommitteeAddress);
+    const sequencerUrl = process.env.SEQUENCER_URL || 'http://zkevm-json-rpc:8123';
+    console.log('SEQUENCER_URL:', sequencerUrl);
+
     // Get the network argument from the command line
     const network = process.argv[2];
     let networkURL;
@@ -41,21 +36,17 @@ async function main() {
     // Load deployer
     const deployer = new ethers.Wallet(privateKey, currentProvider);
 
-    const requiredAmountOfSignatures = 1;
-    const urls = [dataCommitteeUrl];
-    const addrsBytes = dataCommitteeAddress;
-    const cdkDACContract = new ethers.Contract(dataCommitteeContractAddress, dataCommitteeContractJson.abi, deployer);
-    const tx = await cdkDACContract.setupCommittee(
-        requiredAmountOfSignatures,
-        urls,
-        addrsBytes,
-    );
+    const zkEVMContract = new ethers.Contract(zkEVMContractAddress, zkEVMContractJson.abi, deployer);
+    const currentUrl = await zkEVMContract.trustedSequencerURL();
+    console.log(`current trustedSequencerUrl: ${currentUrl}\n`);
+
+    const tx = await zkEVMContract.setTrustedSequencerURL(sequencerUrl);
     console.log('Transaction hash:', tx.hash);
     // Wait for receipt
     const receipt = await tx.wait();
     console.log('Transaction confirmed in block:', receipt.blockNumber);
-    const newDaMember = await cdkDACContract.members(0);
-    console.log('new DA member: ', newDaMember);
+    const newUrl = await zkEVMContract.trustedSequencerURL();
+    console.log(`new trustedSequencerUrl: ${newUrl}`);
 }
 
 main()
